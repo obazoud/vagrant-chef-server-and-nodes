a4fc9bbce3fbaee64373665a56b914e269a1bfc9
Olivier Bazoud <olivier.bazoud@gmail.com>
2014-02-16 23:33:56 +0100

    Fix copy pem

--- copy-pem.sh
+++ copy-pem.sh
@@ -11,12 +11,11 @@ vagrant ssh chef_server -c "sudo rm -f /tmp/chef-validator.pem; sudo cp /etc/che
 scp -F .vagrant.chef_server.ssh.config chef_server:/tmp/admin.pem .
 scp -F .vagrant.chef_server.ssh.config chef_server:/tmp/chef-validator.pem .
 
-
 scp -F .vagrant.chef_workstation.ssh.config admin.pem chef_workstation:/tmp/admin.pem
 scp -F .vagrant.chef_workstation.ssh.config chef-validator.pem chef_workstation:/tmp/chef-validator.pem
 vagrant ssh chef_workstation -c "sudo chown root:root /tmp/admin.pem; sudo mv /tmp/admin.pem /root/.chef/admin.pem"
 vagrant ssh chef_workstation -c "sudo chown root:root /tmp/chef-validator.pem; sudo mv /tmp/chef-validator.pem /root/.chef/chef-validator.pem"
 
-
+vagrant ssh chef_node -c "sudo rm -f /tmp/chef-validator.pem; sudo rm -f /etc/chef/chef-validator.pem"
 scp -F .vagrant.chef_node.ssh.config chef-validator.pem chef_node:/tmp/chef-validator.pem
 vagrant ssh chef_node -c "sudo chown root:root /tmp/chef-validator.pem; sudo mv /tmp/chef-validator.pem /etc/chef/chef-validator.pem"
